fd63bb1849d3b823abe1a084644ccd26a92928ee
BukanAhmadFauzi <achmadchocs96@gmail.com>
2021-03-01 17:10:06 +0700

    pertemuan4

---
+++ resources/views/tryblade/child.blade.php
@@ -0,0 +1,15 @@
+@extends('tryblade.layout')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+    <p>This is appended to the master sidebar</p>
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
+<x-alert>
+    <strong>Whoops!</strong>
+</x-alert>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/admin', [AdminController::class, 'index']);
+
+//Route::resource('/user', [AdminController::class]);
+
+Route::get('/view', function () {
+    return view('tryblade.child');
 });
